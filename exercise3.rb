ddf22dd50489ecb28d44f79408609d26993aef4b
Alec Bollard <alecbollard@gmail.com>
2015-08-18 14:48:23 -0400

    finished a program that asks for a users name and returns, as well as asks for a user's age and returns (probable) birth year

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,8 @@
+# asks for name, then returns name
 puts "What is your name?"
+name = gets.chomp
+puts "Hi #{name}!"
+# asks for age, then determines birth year (albeit incorrectly sometimes)
+puts "How old are you?"
+birth_year = 2015 - gets.to_i
+puts "You were (probably) born in #{birth_year}!"
